46d90456067203d85967083d14b3859d7f692e02
Eyhab333 <107456407+Eyhab333@users.noreply.github.com>
2023-05-13 19:53:41 +0300

    g

--- backend/products.js
+++ backend/products.js
@@ -5,7 +5,8 @@ const products = [
     description:
       "Lorem ipsum, dolor sit amet consectetur adipisicing elite. Sequi, perferendis beatae asperiores.",
     price: 100,
-    imageLink: "",
+    imageLink:
+      "https://res.cloudinary.com/dbtklbenn/image/upload/v1683991215/online-store/Photo_by_Yash_Parashar_on_Unsplash3_g29dsu.jpg",
   },
   {
     id: 2,
@@ -13,7 +14,8 @@ const products = [
     description:
       "Lorem ipsum, dolor sit amet consectetur adipisicing elite. Sequi, perferendis beatae asperiores.",
     price: 200,
-    imageLink: "",
+    imageLink:
+      "https://res.cloudinary.com/dbtklbenn/image/upload/v1683991199/online-store/Photo_by_VVS_on_Unsplash_shuzv5.jpg",
   },
   {
     id: 3,
@@ -21,7 +23,8 @@ const products = [
     description:
       "Lorem ipsum, dolor sit amet consectetur adipisicing elite. Sequi, perferendis beatae asperiores.",
     price: 300,
-    imageLink: "",
+    imageLink:
+      "https://res.cloudinary.com/dbtklbenn/image/upload/v1683991193/online-store/Photo_by_Tadeusz_Lakota_on_Unsplash2_masoz7.jpg",
   },
   {
     id: 4,
@@ -29,7 +32,8 @@ const products = [
     description:
       "Lorem ipsum, dolor sit amet consectetur adipisicing elite. Sequi, perferendis beatae asperiores.",
     price: 400,
-    imageLink: "",
+    imageLink:
+      "https://res.cloudinary.com/dbtklbenn/image/upload/v1683991193/online-store/Photo_by_Tadeusz_Lakota_on_Unsplash2_masoz7.jpg",
   },
 ];
 module.exports = products;
